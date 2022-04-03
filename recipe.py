e3110feb6a097704639c84806c22e8b97ff658df
wilbertwhite <wilbertcwhite@gmail.com>
2022-04-03 15:40:21 -0400

    api testing

--- recipe.py
+++ recipe.py
@@ -1,21 +1,16 @@
-import flask
 import requests
-import json
-
-
-app = flask.Flask(__name__)
 
 BASE_URL = "https://api.edamam.com/api/recipes/v2"
 APP_KEY = "6a51ffd51278380abcfcc9067cbacb1f"
 APP_Id = "2707c6f4"
 
 params = {
-    "app_key": APP_KEY,
-    "app_id": APP_Id,
+    "type": "public",
     "q": "chicken",
+    "app_id": APP_Id,
+    "app_key": APP_KEY,
 }
 
-response = requests.get(BASE_URL, params=params)
-response_json = response.json()["recipe"]
+response = requests.get(BASE_URL, params=params).json()
 
-print(response_json)
+print(response)
