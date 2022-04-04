3e7d65826b2b35e9c990bfcf1448d9d2b16046df
wilbertwhite <wilbertcwhite@gmail.com>
2022-04-04 12:11:38 -0400

    hid secret variables

--- recipe.py
+++ recipe.py
@@ -1,14 +1,16 @@
 import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 BASE_URL = "https://api.edamam.com/api/recipes/v2"
-APP_KEY = "6a51ffd51278380abcfcc9067cbacb1f"
-APP_Id = "2707c6f4"
 
 params = {
     "type": "public",
     "q": "chicken",
-    "app_id": APP_Id,
-    "app_key": APP_KEY,
+    "app_id": os.getenv("APP_ID"),
+    "app_key": os.getenv("APP_KEY"),
 }
 
 response = requests.get(BASE_URL, params=params).json()
